15c2d9edccad090cd67b033f24a43c544e5cba3e
Zhiyuan He <362583303@qq.com>
2019-03-18 20:27:24 +0800

    Update build_data.py

--- chapter_11/build_data.py
+++ chapter_11/build_data.py
@@ -84,7 +84,7 @@ def data_writer(input_dir, output_file):
   output_dir = os.path.dirname(output_file)
   try:
     os.makedirs(output_dir)
-  except os.error, e:
+  except os.error:
     pass
 
   images_num = len(file_paths)
